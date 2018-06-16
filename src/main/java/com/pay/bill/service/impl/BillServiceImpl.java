e2caa0b2eeb885b529788eebaab679640ecf72fa
cookies-run <yuyitaojs@163.com>
2018-06-16 08:24:16 +0800

    关闭账单
    关闭账单

--- src/main/java/com/pay/bill/service/impl/BillServiceImpl.java
+++ src/main/java/com/pay/bill/service/impl/BillServiceImpl.java
@@ -2,7 +2,9 @@ package com.pay.bill.service.impl;
 
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
+import com.alipay.api.request.AlipayEcoEduKtBillingModifyRequest;
 import com.alipay.api.request.AlipayTradeCloseRequest;
+import com.alipay.api.response.AlipayEcoEduKtBillingModifyResponse;
 import com.alipay.api.response.AlipayTradeCloseResponse;
 import com.pay.alipay.utils.PrivateKeySignature;
 import com.pay.bill.service.BillService;
@@ -32,17 +34,32 @@ public class BillServiceImpl implements BillService {
 
         //循环关闭账单
         for(StudentBill bean : studentBills){
-            AlipayTradeCloseRequest request = new AlipayTradeCloseRequest();
+            AlipayEcoEduKtBillingModifyRequest request = new AlipayEcoEduKtBillingModifyRequest();
             request.setBizContent("{" +
                     "\"trade_no\":\""+bean.getOrderNo()+"\"," +
                     "\"out_trade_no\":\""+bean.getOutTradeNo()+"\"," +
+                    "\"status\":\"2\"" +
+//                    "\"status\":\"2\"," +
+//                    "\"fund_change\":\"Y\"," +
+//                    "\"refund_amount\":200.12," +
+//                    "\"refund_reason\":\"正常退款\"," +
+//                    "\"out_request_no\":\"HZ01RF001\"," +
+//                    "\"buyer_logon_id\":\"159****5620\"," +
+//                    "\"gmt_refund\":\"2015-11-27 15:45:57\"," +
+//                    "\"buyer_user_id\":\"2088101117955611\"," +
+//                    "\"refund_detail_item_list\":{" +
+//                    "\"fund_channel\":\"ALIPAYACCOUNT\"," +
+//                    "\"amount\":12.00," +
+//                    "\"real_amount\":12.00" +
+//                    "}" +
                     "  }");
+
             try {
-                AlipayTradeCloseResponse response = alipayClient.execute(request);
+                AlipayEcoEduKtBillingModifyResponse response = alipayClient.execute(request);
                 if(response.isSuccess()){
                     System.out.println("账单关闭成功");
                 } else {
-                    System.out.println("账单关闭失败");
+                    System.out.println("账单关闭失败"+response.getMsg());
                 }
             } catch (AlipayApiException e) {
                 e.printStackTrace();
