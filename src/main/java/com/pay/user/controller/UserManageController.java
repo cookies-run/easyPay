f4f938e6ec5ed9a5501aad0afcd0d2c63a1736d5
jaycaoyang <604930051@qq.com>
2018-06-30 20:59:37 +0800

    用户修改密码
    修改密码

---
+++ src/main/java/com/pay/user/controller/UserManageController.java
@@ -0,0 +1,28 @@
+package com.pay.user.controller;
+
+import com.pay.core.entity.JsonResult;
+import com.pay.user.entity.User;
+import com.pay.user.service.UserManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by cy on 2018/6/18.
+ */
+@Controller
+public class UserManageController {
+    @Autowired
+    UserManageService userManageService;
+    @RequestMapping("editUserPwd")
+    @ResponseBody
+    public JsonResult editUserPwd(User user){
+        return userManageService.editUserPwd(user);
+    }
+    @RequestMapping("addChildAccount")
+    @ResponseBody
+    public JsonResult addChildAccount(User user){
+        return userManageService.addChildAccount(user);
+    }
+}
---
+++ src/main/java/com/pay/user/dao/UserManageDao.java
@@ -0,0 +1,15 @@
+package com.pay.user.dao;
+
+import com.pay.user.entity.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by cy on 2018/6/18.
+ */
+@Repository
+public interface UserManageDao {
+    int isExistUser(@Param("phone") String phone, @Param("oldPwd") String oldPwd);
+    int updateOldPwd(@Param("phone") String phone, @Param("newPwd") String newPwd);
+    int addChildAccount(User user);
+}
