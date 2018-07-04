d544718718d7e2310c2de148432ed7cce303db22
WokunWokun <2329238627@qq.com>
2018-07-04 11:17:10 +0800

    修改提示用语，更改角色显示，拼接账单下载地址 (#11)
    * 修改代理商提示语，修改账号角色显示
    
    修改代理商提示语，修改账号角色显示
    
    * 账单下载地址拼接
    
    账单下载地址拼接

--- src/pay/src/app/bills-detail/bills-detail.component.ts
+++ src/pay/src/app/bills-detail/bills-detail.component.ts
@@ -48,8 +48,9 @@ export class BillsDetailComponent implements OnInit {
        }
       this.appService.get('billExport',downloadParams).subscribe(data =>{
               if(data.json().suc){
-                let datas = data.json().data.filePath;
-                window.location.href = `/${datas}`
+                let path = data.json().data.filePath
+                let name = data.json().data.fileName
+                window.location.href = `/downLoadSchoolBill?&fileName=${name}&filePath=${Path}`
               }else {
                   this.msg.error(data.json().msg);
               }
